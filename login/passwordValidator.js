c677e15a07a89eceb17ea27cd29cd88969c480e4
daihyun <judaihyun1@gmail.com>
2019-12-02 12:34:13 +0900

    [feat] Adding password validator

---
+++ login/passwordValidator.js
@@ -0,0 +1,14 @@
+
+
+function passwordValidator(args)
+{
+
+    let msg = '숫자와 영문자 조합으로 9~16자리를 사용해야 합니다.';
+    let rules = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{9,16}$/;
+    if(!rules.test(args)) 
+    {
+        alert(msg);
+        return false;
+    }
+    return true;
+}
---
+++ test/specs/helper_test.js
@@ -0,0 +1,10 @@
+﻿describe('Helper', function(){
+
+  describe('isExist(args)', () => {
+
+    it('args가 undefined, null일 경우 false를 반환한다', function(){
+    })
+
+  })
+ 
+});

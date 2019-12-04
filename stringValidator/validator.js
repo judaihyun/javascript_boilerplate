626675794a410a8925da85e7b67b6a76d6436816
daihyun <judaihyun1@gmail.com>
2019-12-04 16:34:43 +0900

    [feat] email validator

--- stringValidator/validator.js
+++ stringValidator/validator.js
@@ -16,3 +16,17 @@ function passwordValidator(args, opt)
     }
     return true;
 }
+
+
+function emailValidator(args)
+{
+    /*   https://www.w3resource.com/javascript/form/email-validation.php   */
+    let msg = '유효하지 않은 이메일 주소입니다.';
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(args))
+    {
+        return true;
+    }
+
+    //alert(msg);
+    return false;
+}
--- test/specs/stringValidator_test.js
+++ test/specs/stringValidator_test.js
@@ -1,8 +1,8 @@
 ﻿describe('stringValidator', function(){
 
-  describe('password validator', () => {
+  describe('password validator',() => {
 
-    let testCase = {
+    const testCase = {
       '': false, //empty
       'qwertyuiopasdfgh':false, //16자리
       'qwertyuiopa@fgfh':false, //16자리 with 특수문자
@@ -19,14 +19,41 @@
     };
 
 
-    it('9자리이상 16자리이하, 특수문자, 숫자 포함이 아닐때 false를 반환', () => {
-      for(let prop in testCase)
+    it('9자리이상 16자리이하, 특수문자, 숫자 포함이 아닐때 false를 반환',() => {
+      for(const prop in testCase)
       {
         console.log(`${prop} : ${testCase[prop]} => ` + passwordValidator(prop));
         expect(passwordValidator(prop)).toEqual(testCase[prop]);
       }
-    })
+    });
 
+
+  });
+
+
+  describe('email validator',() =>{
+
+    const testCase = {
+      'mysite@ourearth.com':true,
+      'my.ownsite@ourearth.org':true,
+      'mysite@you.me.net':true,
+      'mysite.ourearth.com':false,  // [@ is not present]
+      'mysite@.com.my':false,  // [ tld (Top Level domain) can not start with dot "." ]
+      '@you.me.net':false,  // [ No character before @ ]
+      'mysite123@gmail.b':false,  // [ ".b" is not a valid tld ]
+      'mysite@.org.org':false,  // [ tld can not start with dot "." ]
+      '.mysite@mysite.org':false,  // [ an email should not be start with "." ]
+      'mysite()*@gmail.com':false,  // [ here the regular expression only allows character, digit, underscore, and dash ]
+      'mysite..1234@yahoo.com':false,  // [double dots are not allowed]
+    }
+
+    it('email validation',() =>{
+      for(const prop in testCase)
+      {
+        console.log(`${prop} : ${testCase[prop]} => ` + emailValidator(prop));
+        expect(emailValidator(prop)).toEqual(testCase[prop]);
+      }
+    });
 
   });
  

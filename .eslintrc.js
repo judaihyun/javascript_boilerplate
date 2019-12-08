8066dafc94c889e105053740c0d8adfef6f4e132
daihyun <judaihyun1@gmail.com>
2019-12-08 17:22:08 +0900

    [config] install eslint

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	env: {
+	  browser: true,
+	  es6: true,
+	},
+	extends: [
+	  'airbnb-base',
+	],
+	globals: {
+	  Atomics: 'readonly',
+	  SharedArrayBuffer: 'readonly',
+	},
+	parserOptions: {
+	  ecmaVersion: 2018,
+	  sourceType: 'module',
+	},
+	rules: {
+		"indent": [
+			2,
+			"tab"
+		],
+		"semi": [
+			"error",
+			"always"
+		],
+		"one-var":[2,
+		  {
+			"var": "never"
+		  }
+		],
+		"func-names": ["error", "never"],
+		"no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
+		"prefer-arrow-callback": [ "error", { "allowNamedFunctions": false} ],
+		"no-tabs":0,
+		"no-console":"off",
+		"linebreak-style":0,
+		"no-trailing-spaces": 0,
+		"keyword-spacing": 0,
+		"no-unused-vars": 1,
+		"space-before-function-paren": 0,
+		"eol-last": 0,
+		"consistent-return": ["error", { "treatUndefinedAsUnspecified": false }],
+		"padded-blocks": ["error", { "blocks": "never" }],
+	}
+  };

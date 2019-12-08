db0508b1ac78ed42fc1bdb31d2434009d35fa8f9
daihyun <judaihyun1@gmail.com>
2019-12-08 17:35:03 +0900

    [style] eslint apply

--- stringValidator/validator.js
+++ stringValidator/validator.js
@@ -1,41 +1,34 @@
 
+function passwordValidator(args, opt) {
+	const obj = opt || {
+		min: 9, max: 16,
+	}
 
-function passwordValidator(args, opt)
-{
-    let obj = opt || {
-        min: 9, max: 16
-    }
-    
-    let msg = '숫자와 영문자 조합으로 9~16자리를 사용해야 합니다.';
-    let rules = new RegExp("^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{"+obj.min+","+obj.max+"}$");
-    
-    if(!rules.test(args)) 
-    {
-        //alert(msg);
-        return false;
-    }
-    return true;
+	const msg = '숫자와 영문자 조합으로 9~16자리를 사용해야 합니다.';
+	const rules = new RegExp("^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{" + obj.min + "," + obj.max + "}$");
+
+	if (!rules.test(args)) {
+		// alert(msg);
+		return false;
+	}
+	return true;
 }
 
-function telValidator(args)
-{
-    let msg = '유효하지 않는 전화번호입니다.';
-    if(/^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}/.test(args))
-    {
-        return true;
-    }
-    //alert(msg);
-    return false;
+function telValidator(args) {
+	const msg = '유효하지 않는 전화번호입니다.';
+	if (/^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}/.test(args)) {
+		return true;
+	}
+	// alert(msg);
+	return false;
 }
 
-function emailValidator(args)
-{
-    let msg = '유효하지 않는 이메일입니다.';
-    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(args))
-    {
-        return true;
-    }
+function emailValidator(args) {
+	const msg = '유효하지 않는 이메일입니다.';
+	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(args)) {
+		return true;
+	}
 
-    //alert(msg);
-    return false;
+	// alert(msg);
+	return false;
 }
